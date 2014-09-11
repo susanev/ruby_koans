ccee66270af37c438ec522358b702e59c836aa9e
Dean Hudson <dean@ero.com>
2014-09-11 15:17:56 -0700

    Set up path for 9/12/14, split methods into beg. and adv.

--- src/about_methods.rb
+++ src/about_methods.rb
@@ -15,47 +15,6 @@ class AboutMethods < Neo::Koan
     assert_equal __(5), result
   end
 
-  # (NOTE: We are Using eval below because the example code is
-  # considered to be syntactically invalid).
-  def test_sometimes_missing_parentheses_are_ambiguous
-    #--
-    eval "assert_equal 5, my_global_method(2, 3)" # REMOVE CHECK # __
-    if false
-      #++
-    eval "assert_equal 5, my_global_method 2, 3" # ENABLE CHECK # __
-      #--
-    end
-    #++
-    #
-    # Ruby doesn't know if you mean:
-    #
-    #   assert_equal(5, my_global_method(2), 3)
-    # or
-    #   assert_equal(5, my_global_method(2, 3))
-    #
-    # Rewrite the eval string to continue.
-    #
-  end
-
-  # NOTE: wrong number of arguments is not a SYNTAX error, but a
-  # runtime error.
-  def test_calling_global_methods_with_wrong_number_of_arguments
-    exception = assert_raise(___(ArgumentError)) do
-      my_global_method
-    end
-    #--
-    pattern = "wrong (number|#) of arguments"
-    #++
-    assert_match(/#{__(pattern)}/, exception.message)
-
-    exception = assert_raise(___(ArgumentError)) do
-      my_global_method(1,2,3)
-    end
-    assert_match(/#{__(pattern)}/, exception.message)
-  end
-
-  # ------------------------------------------------------------------
-
   def method_with_defaults(a, b=:default_value)
     [a, b]
   end
@@ -63,19 +22,6 @@ class AboutMethods < Neo::Koan
   def test_calling_with_default_values
     assert_equal [1, __(:default_value)], method_with_defaults(1)
     assert_equal [1, __(2)], method_with_defaults(1, 2)
-  end
-
-  # ------------------------------------------------------------------
-
-  def method_with_var_args(*args)
-    args
-  end
-
-  def test_calling_with_variable_arguments
-    assert_equal __(Array), method_with_var_args.class
-    assert_equal __([]), method_with_var_args
-    assert_equal __([:one]), method_with_var_args(:one)
-    assert_equal __([:one, :two]), method_with_var_args(:one, :two)
   end
 
   # ------------------------------------------------------------------
@@ -115,47 +61,4 @@ class AboutMethods < Neo::Koan
     assert_equal __(12), self.my_method_in_the_same_class(3,4)
   end
 
-  # ------------------------------------------------------------------
-
-  def my_private_method
-    "a secret"
-  end
-  private :my_private_method
-
-  def test_calling_private_methods_without_receiver
-    assert_equal __("a secret"), my_private_method
-  end
-
-  def test_calling_private_methods_with_an_explicit_receiver
-    exception = assert_raise(___(NoMethodError)) do
-      self.my_private_method
-    end
-    assert_match /#{__("method `my_private_method'")}/, exception.message
-  end
-
-  # ------------------------------------------------------------------
-
-  class Dog
-    def name
-      "Fido"
-    end
-
-    private
-
-    def tail
-      "tail"
-    end
-  end
-
-  def test_calling_methods_in_other_objects_require_explicit_receiver
-    rover = Dog.new
-    assert_equal __("Fido"), rover.name
-  end
-
-  def test_calling_private_methods_in_other_objects
-    rover = Dog.new
-    assert_raise(___(NoMethodError)) do
-      rover.tail
-    end
-  end
 end
